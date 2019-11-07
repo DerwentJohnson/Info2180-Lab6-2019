cb42e17993a72fced7c681a89eb46b2da9329ce6
unknown <derwent.johnson@gmail.com>
2019-11-07 18:28:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    var search = document.getElementById("search");
+    this.console.log(search)
+search.addEventListener("click",() => {
+    // var xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange = () => {
+    //     console.log(this.readyState);
+    //     if (this.readyState == 4 && this.status == 200){
+    //         console.log(this.responseText);
+    //     }
+    // } 
+    // xhr.open("GET","superheroes.php?q=",true);
+    // xhr.send();
+    this.console.log("ok")
+})
+}
